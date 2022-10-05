4edc770ff46ca6ff8c9e44db75424c9667dbf55e
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2022-10-05 12:47:21 +0530

    added movies

--- movie-recommendation/src/App.js
+++ movie-recommendation/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+const genres = {
+  Thriller:[
+    {"name": "Se7en", "rating": "9/10"},
+    {"name": "Memories of Murder", "rating": "9/10"},
+    {"name": "Goodfellas", "rating": "8.8/10"},
+    {"name": "Godfather II", "rating": "8.7/10"},
+    {"name": "Prisoners", "rating": "8/10"}
+  ],
+  Feels: [
+    { name: "Pursuit of Happyness", rating: "9.3/10" },
+    { name: "Forest Gump", rating: "8.8/10" },
+    { name: "Spirited Away", rating: "8.5/10" }
+  ],
+  Drama: [
+    { name: "Birdman", rating: "9.5/10" },
+    { name: "The Portrait of a Lady on Fire", rating: "8.8/10" },
+    { name: "Marriage Story", rating: "8/10" }
+  ]
+};
 
 function App() {
-  return (
+
+  const [selectedCategoryItem, setCategoryItem] = useState("Thriller");
+  const genresArray = Object.keys(genres);
+  function categoryClickHandler(genresCategory) {
+    setCategoryItem(genresCategory);
+  }
+  
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 style={{ padding: "5px", margin: "1rem", backgroundColor: "red"}}>Movies 🎥</h1>
+      <h4>Select a category to view recommendations</h4>
+      {genresArray.map((genres) => {
+        return (
+          <button style={{ padding: "5px", margin: "1rem"}} key={genres} onClick={() => categoryClickHandler(genres)}> {genres}
+          </button>
+        );
+      })}
+      <ul>
+        {genres[selectedCategoryItem].map((movie) => (
+          <div style={{ padding:"1rem", border: "1px solid black", width: "400px",
+                        margin:"2rem auto" }}>
+          <li id="listItem" key={movie.name} style={{ listStyleType: "none"}}>
+            <div id="name">Name: {movie.name}</div>{" "}
+            <div id="rating">Rating: {movie.rating}</div>
+          </li>
+        </div>
+        ))}
+      </ul>
+  </div>
   );
 }
 
